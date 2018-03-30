c382a63f7c00738a8294b3b6edcc833c3dbe68f6
Meister <meistermilad.a@gmail.com>
2018-03-31 00:29:33 +0200

    hotfix

--- bin/index.js
+++ bin/index.js
@@ -2,12 +2,11 @@
 
 var path = require('path');
 var fs = require('fs');
-var userPath = process.cwd();
-var configPath = path.resolve(userPath, 'softpack.config.js');
+var configPath = path.resolve(process.cwd(), 'softpack.config.js');
 var config;
 var contents;
 var boilerPlates = [];
-fs.readdirSync(path.resolve(__dirname, 'boilerplates')).forEach(e => {
+fs.readdirSync(path.resolve(__dirname, 'lib')).forEach(e => {
     boilerPlates.push(e.replace('.js', ''))
 })
 var softpack = require('./../index');
